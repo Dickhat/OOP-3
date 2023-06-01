642936a92b5f970f9d7993ce524da584ffc68e96
Даниил Богданов <danya.bogdanov.03@mail.ru>
2023-06-01 23:40:55 +0300

    Reworked

--- OOP 3/OOP 3.cpp
+++ OOP 3/OOP 3.cpp
@@ -444,17 +444,17 @@ int main()
 	HPEN PenGreen = CreatePen(PS_SOLID, 3, RGB(0, 255, 0));
 	HPEN PenRed = CreatePen(PS_SOLID, 3, RGB(255, 0, 0));
 	HPEN PenBlue = CreatePen(PS_SOLID, 3, RGB(0, 0, 200));
-	HPEN PenWhite = CreatePen(PS_SOLID, 3, RGB(0, 0, 0));
+	HPEN PenWhite = CreatePen(PS_SOLID, 3, RGB(255, 255, 255));
+	HPEN PenGray = CreatePen(PS_SOLID, 3, RGB(120, 120, 120));
 
 	vertical_Tower_1 green_tw_1(400, 200, PenGreen);
 	vertical_Tower_2 green_tw_2(600, 200, PenGreen);
 
 	left_construction_Tower cons_tw_1(200, 400, PenBlue);
-	right_construction_Tower cons_tw_2(400, 400, PenRed);
+	right_construction_Tower cons_tw_2(400, 400, PenGray);
 
 	ball bill_1(200, 200, PenRed, 10);
 	ball bill_2(800, 400, PenRed, 40);
-	ball bill_3(1000, 400, PenRed, 70);
 	bill_1.set_visible(PenRed);
 	bill_2.set_visible(PenRed);
 
@@ -469,31 +469,55 @@ int main()
 	BALL[0] = &bill_1;
 	BALL[1] = &bill_2;
 
+	//Матрица перехода
 	Tower* collisions[4][2]= 
 	{
-		{new vertical_Tower_1(green_tw_1.Get_X(),green_tw_1.Get_Y(), PenRed), new vertical_Tower_1(1000, 1000,PenWhite)},
-		{new vertical_Tower_2(green_tw_2.Get_X(),green_tw_2.Get_Y(), PenRed), new vertical_Tower_1(green_tw_2.Get_X(),green_tw_2.Get_Y(), PenRed)},
-		{new left_construction_Tower(cons_tw_1.Get_X(),cons_tw_1.Get_Y(), PenRed),new vertical_Tower_1(cons_tw_1.Get_X(),cons_tw_1.Get_Y(), PenRed)},
-		{new right_construction_Tower(cons_tw_2.Get_X(),cons_tw_2.Get_Y(), PenRed), new vertical_Tower_1(cons_tw_2.Get_X(),cons_tw_2.Get_Y(), PenRed)}
+		{new vertical_Tower_1(green_tw_1.Get_X(),green_tw_1.Get_Y(), PenRed),		//Красная башня
+		new vertical_Tower_1(1000, 1000,PenWhite)},									//Уничтоженная башня
+		{new vertical_Tower_2(green_tw_2.Get_X(),green_tw_2.Get_Y(), PenRed),		//Красная башня с правой пристройкой 
+		new vertical_Tower_1(green_tw_2.Get_X(),green_tw_2.Get_Y(), PenRed)},		//Красная башня без пристройки
+		{new left_construction_Tower(cons_tw_1.Get_X(),cons_tw_1.Get_Y(), PenRed),	//Красная башня с левой пристройкой
+		new vertical_Tower_1(cons_tw_1.Get_X(),cons_tw_1.Get_Y(), PenRed)},			//Красная башня без пристройки
+		{new right_construction_Tower(cons_tw_2.Get_X(),cons_tw_2.Get_Y(), PenRed),	//Красная башня с правой пристройкой
+		new vertical_Tower_1(cons_tw_2.Get_X(),cons_tw_2.Get_Y(), PenRed)}			//Красная башня без пристройки
 	};
 
+	//for использование матрицы перехода
 	for(int i = 0; i < Tow.size(); ++i)
 	{
-		Pcur = Tow[i];
-		Pcur->set_visible(Pcur->pen_color());
-		Pcur->Drag();
+		Pcur = Tow[i];									//Указатель на ПК
+		Pcur->set_visible(Pcur->pen_color());			//Отображение фигуры
+		Sleep(1000);
+		Pcur->Drag();									//Буксировка фигуры
+
+		//for Поиск коллизий
 		for (int j = 0; j < BALL.size(); ++j)
 		{
+			//if Произошла коллизия
 			if (check(i, j, Tow, BALL))
-			{
-				Pcur->set_invisible();
-				Pcur = collisions[i][j];
-				Pcur->set_visible(Pcur->pen_color());
+			{	
+				Pcur->set_invisible();					//Убрать старый объект
+
+				//Появление новой фигуры на месте старой
+				collisions[i][j]->Move_To(Pcur->Get_X(), Pcur->Get_Y());
+
+				Pcur = collisions[i][j];				//Взять объект из матрицы перехода
+				Pcur->set_visible(Pcur->pen_color());	//Отобразить новый объект
+
 				Sleep(1000);
-				Pcur->set_invisible();
-			}
-		}
-	}
+
+				//if Проверка, что двигаем не пустой объект
+				if(i != 0 && j!= 1)
+					Pcur->Drag();						//Буксировка фигуры
+
+				break;
+			}//if
+		}//for
+
+		BALL[0]->set_visible(PenRed);			//Повторная отрисовка шара
+		BALL[1]->set_visible(PenRed);			//Повторная отрисовка шара
+		Pcur->set_invisible();					//Убрать новый объект
+	}//for
 
 	DeleteObject(Pen);
 
@@ -511,6 +535,6 @@ int check(int i, int j,vector <Tower*>& Tow, vector <ball*>& BALL)
 		|| Tow[i]->get().start_Y < BALL[j]->get().end_Y)
 		return 0;
 	else
-		return (j + 1);
+		return (j + 1);	//Вернуть номер объекта столкновения
 	
 };
